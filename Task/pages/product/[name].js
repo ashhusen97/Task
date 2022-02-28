29b18af44d2b666288bc7e44aa16d3657d823cdb
aashir.hussain <aashir.hussain@unilever.com>
2022-02-28 21:14:09 +0500

    finish task

---
+++ Task/pages/product/[name].js
@@ -0,0 +1,47 @@
+import {
+  Badge,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Chip,
+  Container,
+  Grid,
+  Paper,
+  Typography,
+} from "@mui/material";
+import { useRouter } from "next/router";
+import React from "react";
+
+export default function ProductScreen({ data }) {
+  // const router = useRouter();
+  // router;
+  // console.log(first);
+  // const { name } = router.query;
+  // console.log("ssss", name);
+  return (
+    <Grid container spacing={2} padding={10}>
+      <Grid item xs={12} sm={6} md={2} lg={6}>
+        <Card elevation={10} style={{ padding: 10 }}>
+          <Typography variant="h6">{data.id}</Typography>
+          <Typography variant="h3">{data.name}</Typography>
+          <Typography variant="small">{data.description}</Typography>
+          <Container>
+            <Chip label={data.status} />
+          </Container>
+        </Card>
+      </Grid>
+    </Grid>
+  );
+}
+export async function getServerSideProps(context) {
+  const res = await fetch(
+    `https://hungry-skinny-cappelletti.glitch.me/tenants/${context.query.name}`
+  );
+  const data = await res.json();
+  console.log(data);
+  return {
+    props: { data: data }, // will be passed to the page component as props
+  };
+}
