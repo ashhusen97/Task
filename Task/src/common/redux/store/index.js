a743cc2ff5766599206af614d39ed701473bd1c7
aashir.hussain <aashir.hussain@unilever.com>
2022-02-28 21:16:04 +0500

    finish task

--- Task/src/common/redux/store/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { combineReducers } from "redux";
-import tenantReducer from "../reducers/tenantReducer";
-
-export const store = configureStore({
-  reducer: combineReducers({
-    tenants: tenantReducer,
-  }),
-});
